2d1868561dbb5b15148c45cc1bb6d6a6649ab9cc
toknowjoyman <syko.inc@gmail.com>
2017-11-25 23:45:17 -0800

    added listener

--- qwebchannel.js
+++ qwebchannel.js
@@ -429,3 +429,7 @@ var jshelper;
 new QWebChannel(qt.webChannelTransport, function (channel) {
     jshelper = channel.objects.jshelper;
 });
+
+document.getElementById("myBtn").addEventListener("click", function(){
+    jshelper.markerMoved("candy")
+});
