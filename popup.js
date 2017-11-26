68fdcc3f49fc9a2df4609dcc8ae2afd9b39631f6
toknowjoyman <syko.inc@gmail.com>
2017-11-26 01:41:07 -0800

    Update popup.js
    // iframe.contentWindow.document.documentElement.innerHTML
    // var t = document.getElementByTagName("iframe").src.atob()
    
    // t.getElementByTagName("p")addEventListener("click", function(){
        
    // });

--- popup.js
+++ popup.js
@@ -2,11 +2,6 @@ var jshelper;
 new QWebChannel(qt.webChannelTransport, function (channel) {
     jshelper = channel.objects.jshelper;
 });
-
-
-// iframe.contentWindow.document.documentElement.innerHTML
-// var t = document.getElementByTagName("iframe").src.atob()
 jshelper.markerMoved("candy")
-// t.getElementByTagName("p")addEventListener("click", function(){
-    
-// });
+
+
