4a777200e5589c464e989af34bb8bc6722c8a1f2
toknowjoyman <syko.inc@gmail.com>
2017-11-26 01:37:00 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,6 +3,9 @@ new QWebChannel(qt.webChannelTransport, function (channel) {
     jshelper = channel.objects.jshelper;
 });
 
-document.getElementById("myBtn").addEventListener("click", function(){
+
+// iframe.contentWindow.document.documentElement.innerHTML
+var t = document.getElementByTagName("iframe").src.atob()
+t.getElementByTagName("p")addEventListener("click", function(){
     jshelper.markerMoved("candy")
 });
